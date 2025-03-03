4d47485ec22e3af9a9a06067be7f863bc5de7258
alexNg <ntriet0612@gmail.com>
2025-03-03 10:37:33 +0700

    init base structure and add core util

---
+++ src/core/vigenere-cipher.ts
@@ -0,0 +1,84 @@
+
+type VigenereCipher = {
+  encode: (plainText: string, key: string, alphabet: string) => string,
+  decode: (cipherText: string, key: string, alphabet: string) => string
+}
+
+type HashMap = {
+  [key: string]: number
+}
+
+const markIndexChar = (alphabet: string) => {
+  let hashMap: HashMap = {};
+  for (let i = 0; i < alphabet.length; i++) {
+    hashMap[alphabet[i]] = i;
+  }
+
+  return hashMap;
+}
+
+const vigenereCipherEncoder = (plainText: string, key: string, alphabet: string): string => {
+  let pText = 0;
+  let pKey = 0;
+  let cipherText = "";
+
+  const hashMap = markIndexChar(alphabet);
+
+  while (pText < plainText.length) {
+    if (plainText[pText] === " ") {
+      cipherText += " ";
+      pText++;
+      continue
+    }
+
+    const cipherCharIndex = (hashMap[plainText[pText]] + hashMap[key[pKey]]) % alphabet.length;
+    cipherText += alphabet[cipherCharIndex];
+
+    pText++;
+    pKey++;
+    if (pKey === key.length) {
+      pKey = 0;
+    }
+  }
+  return cipherText;
+}
+
+
+const vigenereCipherDecoder = (cipherText: string, key: string, alphabet: string): string => {
+
+  let pText = 0;
+  let pKey = 0;
+  let decodedText = "";
+
+  const hashMap = markIndexChar(alphabet);
+
+  while (pText < cipherText.length) {
+    if (cipherText[pText] === " ") {
+      decodedText += " ";
+      pText++;
+      continue
+    }
+
+    const decodedCharIndex = (hashMap[cipherText[pText]] - hashMap[key[pKey]] + alphabet.length) % alphabet.length;
+    decodedText += alphabet[decodedCharIndex];
+
+    pText++;
+    pKey++;
+    if (pKey === key.length) {
+      pKey = 0;
+    }
+  }
+
+  return decodedText;
+}
+
+const hashedText = vigenereCipherEncoder("minh triet", "cryptii", "zabcdefg@hijklmnopqrstuvwxy");
+const decodedText = vigenereCipherDecoder(hashedText, "cryptii", "zabcdefg@hijklmnopqrstuvwxy");
+
+console.log(hashedText);
+console.log(decodedText);
+
+export const vigenereCipher: VigenereCipher = {
+  encode: vigenereCipherEncoder,
+  decode: vigenereCipherDecoder
+}
